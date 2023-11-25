8bb607eab2df209928039aad0932d5cb489ce2b9
Saeedahamed <63773365+saeedahamed007@users.noreply.github.com>
2023-11-26 00:49:02 +0530

    Update app.py

--- ITG_SCANNER_BHAI/app.py
+++ ITG_SCANNER_BHAI/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from flask_netlify import Netlify
 
 app = Flask(__name__)
-netlify = Netlify(app)
 
 # Load stock symbols from CSV file
 with open('sc_stocks.csv', 'r') as file:
